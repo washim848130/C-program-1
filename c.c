f43110739692694cae5bd83868d4c1ed3608e061
DESKTOP-7VM6LQN\compworld <was>
2024-01-26 11:04:57 +0530

    count

---
+++ c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*int main()
+{
+int n,i;
+printf("enter a number");
+scanf("%d",&n);
+  for(i=1;i<=n;i++){
+    printf(" %d ",i);
+  }
+  return 0 ;
+}*/
+int main()
+{int x>=1,count==0;
+printf("enter a number");
+scanf("%d",&x);
+if(x%10==1)
+printf("%d",count);
+return 0;}
+ 
